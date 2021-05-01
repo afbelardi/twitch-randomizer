ddf53154abb6a89f7042b60875dfc9e03a89133d
Alex <afbelardi@gmail.com>
2021-05-01 18:06:03 -0400

    added pseudocode

---
+++ src/pseudocode.js
@@ -0,0 +1,36 @@
+/*
+
+TWITCH RANDOMIZER 
+
+-Header Title
+- Stream Window/Chat
+- Sidebar (with categories)
+- Randomize Button
+- Ads Div
+
+
+PAGES
+- Main (div with ads, header title)
+
+COMPONENTS
+- Stream window/chat
+- Sidebar (Navigation)
+- Randomize button
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
