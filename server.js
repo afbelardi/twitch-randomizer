1d589e346ab9a78632d41760096a746a142887bb
Joshua <millerjoshua737@gmail.com>
2021-05-05 21:16:17 -0500

    trying to get twitch token access

---
+++ server.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const request = require('request')
+
+const getToken = (url, callback) => {
+
+    const options= {
+        url: process.env.GET_TOKEN,
+        json: true,
+        body: {
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET,
+            grant_type: "client_credentials"
+        }
+    };
+
+    request.post(options, (err, res, body) => {
+        if(err){
+            return console.error(err)
+        }
+        console.log(`Status: ${res.statusCode}`);
+        console.log(body)
+
+        callback(res);
+    })
+}
+
+getToken(process.env.GET_TOKEN, (res) => {
+    console.log(res.body);
+})
